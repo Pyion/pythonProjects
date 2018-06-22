d16aeb30d82805579a887562cd95d6b689ab6ede
Pyion <korneliusz02@gmail.com>
2018-06-22 15:47:19 +0200

    README update

--- stopwatch.py
+++ stopwatch.py
@@ -1,12 +1,11 @@
 import time
 import os
 t = time.time()
-input()
 while 1:
     tc = time.time() - t
-    os.system("clear")
+    #os.system("clear")
     if(int(tc%60<10)):
-        print(str(int(tc//60))+":0"+str(int(tc%60)))
+        print(str(int(tc//60))+":0"+str(int((tc%60)*1000)/1000))
     else:
-        print(str(int(tc//60))+":"+str(int(tc%60)))
-    time.sleep(1)
+        print(str(int(tc//60))+":"+str(int((tc%60)*1000)/1000))
+    time.sleep(0.0003)
