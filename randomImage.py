1880669e139ea6901fe6521ee6c510c20e1c1f9d
Pyion <korneliusz02@gmail.com>
2019-02-27 18:44:33 +0100

    randImage update

--- randomImage.py
+++ randomImage.py
@@ -12,7 +12,22 @@ def randomImage(imageSize):
         arrayRandom[i] = randint(0,256)
     return arrayRandom.reshape((imageSize[0], imageSize[1], 3))
 
+def extendImage(imageArray,dim):
+    newImageArray = numpy.zeros(len(imageArray)*len(imageArray[0])*3*dim*dim, numpy.uint8).reshape((dim*len(imageArray),dim*len(imageArray),3))
+    for i in range(len(imageArray)):
+        for j in range(len(imageArray[i])):
+            for k in range(dim):
+                for l in range(dim):
+                    newImageArray[i*dim+k][j*dim+l] = imageArray[i][j]
+                    #print(i,j,newImageArray[i*dim+k][j*dim+l],imageArray[i][j])
+    print(newImageArray)
+    return newImageArray
+
+
 if(__name__ == '__main__'):
-    imageToGenerate = randomImage((16, 16))
+    imageToGenerate=randomImage((16, 16))
+    imageToGenerateExtended = extendImage(imageToGenerate,64)
     generatedPiImage = Image.fromarray(imageToGenerate, "RGB")
     generatedPiImage.save("randImage.png")
+    generatedPiImage = Image.fromarray(imageToGenerateExtended, "RGB")
+    generatedPiImage.save("randImage2.png")
